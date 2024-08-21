9e5292706818532a0a0827bb5e9ecdbbdc7e8f73
sandy4ka1488 <kukusik1941@gmail.com>
2024-08-21 17:39:03 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let open_btn = document.querySelector('.open-br');
+let popup = document.querySelector('.popup');
+
+
+
+open_btn.addEventListener('click', function() {
+    
+    popup.classList.add('df');
+
+});
+
+document.querySelector('.popup-close').addEventListener('click', function() {
+    popup.classList.remove('df');
+});
+
+
+let popup__link = document.querySelector('.popup-block__link');
+const LINK = 'https://t.me/+tXnTTNIrtRA5M2Uy';
+popup__link.href = LINK;
